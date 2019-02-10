d41b138393f37d326f2db2a37d72c8356e9a2303
pupi1985 <pupi1985@bitbucket.org>
2019-02-10 12:36:51 -0300

    Update plugin metadata

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
-	Plugin Name: Permissions2Categories
-	Plugin URI: https://github.com/kfuchs/permission2categories
-	Plugin Description: Allows permissions to be set for categories.
-	Plugin Version: 1.0.
-	Plugin Date: 2012-12-27
-	Plugin Author: Kirill Fuchs
-	Plugin Author URI: http://codesubstance.com/
-	Plugin License: GPLv2
-	Plugin Minimum Question2Answer Version: 1.5
-	Plugin Update Check URI:
-*/
-
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
     header('Location: ../../');
     exit;
